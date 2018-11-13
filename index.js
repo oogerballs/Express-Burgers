8e7d2a33b0004d32e428589313d8b96e2ebd8b5f
josh9harr <josh9harr@gmail.com>
2018-11-13 13:10:22 -0700

    added index.pug, layout.pug and config.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 console.log("This is running");
+
+var express = require('express');
+var pug = require('pug');
+var path = require('path');
+var config = require('./config.json');
+
+var app = express();
+
+app.set('view engine','pug');
+app.set('views',__dirname+'/views');
+app.use(express.static(path.join(__dirname+'/public')));
+
+app.get('/',function(req,res){
+    res.render('index', {
+        "title":"Home",
+        "config":config
+    });
+});
+
+app.get('/:name',function(req,res){
+    res.render('pages',{
+        "title":req.params.name,
+        "config":config
+    });
+});
+
+
+app.listen(3000);
