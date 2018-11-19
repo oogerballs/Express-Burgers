76808df3bfa4da84c5b9c412bd19eb0360d635e1
josh9harr <josh9harr@gmail.com>
2018-11-19 00:33:42 -0700

    added transitions

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var pug = require('pug');
 var path = require('path');
 var config = require('./config.json');
-
 var app = express();
 
+//var img = require("./Public/Assets/The_Monster.jpg");
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + '/Public')));
 
 app.get('/', function (req, res) {
     res.render('index', {
