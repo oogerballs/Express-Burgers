269679bc8880daa05df87467f5c690d09e106fc3
oogerballs <36011013+oogerballs@users.noreply.github.com>
2018-11-18 20:00:41 -0700

    A lot
    -Added the menu Items
    -added the menu items in the json file
    -added the code that sets the menu items in the index.js
    -added some styling to the menu items div

--- index.js
+++ index.js
@@ -5,31 +5,39 @@ var config = require('./config.json');
 
 var app = express();
 
-app.set('view engine','pug');
-app.set('views',__dirname+'/views');
-app.use(express.static(path.join(__dirname+'/public')));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname + '/public')));
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.render('index', {
-        "title":"Home",
-        "config":config
+        "title": "Home",
+        "config": config
     });
 });
 
-app.get('/directions',function(req,res){
-    res.render('direction',{
-        "title":req.params.name,
-        "config":config
+app.get('/directions', function (req, res) {
+    res.render('direction', {
+        "title": req.params.name,
+        "config": config
     });
 });
 
-app.get('/:name',function(req,res){
-    res.render('pages',{
-        "title":req.params.name,
-        "config":config
+app.get('/:name', function (req, res) {
+    var Title = req.params.name;
+    var page;
+    if (Title == "burgers") {
+        page = 0;
+    } else if (Title == "desserts") {
+        page = 1;
+    } else if (Title == "sides") {
+        page = 2
+    }
+    res.render('pages', {
+        "title": Title,
+        "config": config,
+        "page": page
     });
 });
-
-
 
 app.listen(3000);
