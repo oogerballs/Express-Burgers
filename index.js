2641b1b29fb72fba8baa58353b397f1591af257f
oogerballs <36011013+oogerballs@users.noreply.github.com>
2018-11-19 23:23:12 -0700

    A lot
    added a whole lot of style stuff like the background image, the font, the style with that, and the text boxes

--- index.js
+++ index.js
@@ -12,31 +12,38 @@ app.use(express.static(path.join(__dirname + '/Public')));
 app.get('/', function (req, res) {
     res.render('index', {
         "title": "Home",
-        "config": config
+        "config": config,
+        "descType" : 0
     });
 });
 
 app.get('/directions', function (req, res) {
     res.render('direction', {
         "title": req.params.name,
-        "config": config
+        "config": config,
+        "descType": 1
     });
 });
 
 app.get('/:name', function (req, res) {
     var Title = req.params.name;
     var page;
+    var DescType;
     if (Title == "burgers") {
         page = 0;
+        DescType = 0;
     } else if (Title == "desserts") {
         page = 1;
+        DescType = 1;
     } else if (Title == "sides") {
         page = 2
+        DescType = 2;
     }
     res.render('pages', {
         "title": Title,
         "config": config,
-        "page": page
+        "page": page,
+        "descType": DescType
     });
 });
 
